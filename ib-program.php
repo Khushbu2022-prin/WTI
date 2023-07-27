09874f6a9b69328d2fa577a4df7b5507fd70fb76
ramaprixim <rama.prixim@gmail.com>
2023-07-27 18:11:55 +0530

    padding

--- ib-program.php
+++ ib-program.php
@@ -1,2 +1,112 @@
-<?php include ("header.php") ?>
-<?php include ("footer.php") ?>
+<?php include('header.php')?>
+
+    <div class="preloader">
+        <div class="preloader__image"></div>
+    </div>
+    <div class="page-wrapper">
+        
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div>
+        </div>
+       <!--Page Header Start-->
+       <section class="page-header">
+        <div class="page-header__bg"></div><!-- /.page-header__bg -->
+        <div class="page-header-shape-1"></div><!-- /.page-header-shape-1 -->
+        <div class="page-header-shape-2"></div><!-- /.page-header-shape-2 -->
+        <div class="page-header-shape-3"></div><!-- /.page-header-shape-3 -->
+        <div class="container">
+            <div class="page-header__inner">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>/</span></li>
+                    <li>PARTNERS</li>
+                </ul>
+                <h2>IB PROGRAM</h2>
+            </div>
+        </div>
+    </section>
+    <!--Page Header End-->
+    
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="about__img-box">
+                        <div class="about-img">
+                            <img src="assets/images/resources/about-img-1.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="about__right">
+                        <h2 class="about__title">Introducing brokers (IB)</h2>
+                        <div class="about__icon-box">
+                            <div class="">
+                                <p>Introducing Brokers (IBs) are individuals or entities that act as intermediaries or agents in the financial markets, connecting traders or investors with brokerage firms or financial service providers. They play a crucial role in expanding the client base of brokerage firms and promoting their services to potential customers. IBs facilitate the introduction of clients to the brokerage, earning commissions or compensation in return for their services.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  <section class="real-world">
+            <div class="real-world-shape wow slideInLeft animated" data-wow-delay="100ms" data-wow-duration="2500ms" style="background-image: url(&quot;assets/images/shapes/real-world-shape.png&quot;); visibility: visible; animation-duration: 2500ms; animation-delay: 100ms; animation-name: slideInLeft;"></div>
+            <div class="container">
+                <div class="section-title text-center">
+                    <h2 class="section-title__title">Benefits for IBs</h2>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 wow fadeInUp animated" data-wow-delay="0ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInUp;">
+                        <!--Real World Single-->
+                        <div class="real-world__single">
+                            <h2 class="real-world__title">Additional Revenue Stream
+                            </h2>
+                            <p style="color:aliceblue">IBs earn commissions or compensation from the brokerage firm based on the trading activity of the clients they refer. This can serve as an additional revenue stream for IBs.</p>
+                            <div class="real-world__icon-box">
+                                <span class="icon-wealth"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 300ms; animation-name: fadeInUp;">
+                        <!--Real World Single-->
+                        <div class="real-world__single">
+                            <h2 class="real-world__title">No Need for Licensing</h2>
+                            <p style="color:aliceblue">IBs don't need to obtain individual licenses to offer brokerage services since they are not directly executing trades or managing funds.</p>
+                            <br>
+                            <br>
+                            <div class="real-world__icon-box">
+                                <span class="icon-data-analytics"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 600ms; animation-name: fadeInUp;">
+                        <!--Real World Single-->
+                        <div class="real-world__single">
+                            <h2 class="real-world__title">Flexibility and Independence
+                            </h2>
+                            <p style="color:aliceblue"> Introducing Brokers often have the flexibility to operate independently and may choose their own marketing strategies and target markets.</p>
+                            <div class="real-world__icon-box">
+                                <span class="icon-report"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 600ms; animation-name: fadeInUp;">
+                        <!--Real World Single-->
+                        <div class="real-world__single">
+                            <h2 class="real-world__title">Networking Opportunities
+                            </h2>
+                            <p style="color:aliceblue">Building a network of traders and investors can open doors to other business opportunities and collaborations within the financial industry.</p>
+                            <div class="real-world__icon-box">
+                                <span class="icon-report"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+     <?php include('footer.php') ?> 
+
