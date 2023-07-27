92acf0c2e273c78f433d59db16ca9cd2d35c56f9
shubhamprixim <shubham.prixim@gmail.com>
2023-07-27 18:04:57 +0530

    chart

--- charts.php
+++ charts.php
@@ -1,14 +1,14 @@
-<?php include ("header.php") ?>
+<?php include("header.php") ?>
 <div class="preloader">
-        <div class="preloader__image"></div>
+    <div class="preloader__image"></div>
+</div>
+<div class="page-wrapper">
+
+    <div class="stricky-header stricked-menu main-menu">
+        <div class="sticky-header__content"></div>
     </div>
-    <div class="page-wrapper">
-        
-        <div class="stricky-header stricked-menu main-menu">
-            <div class="sticky-header__content"></div>
-        </div>
-       <!--Page Header Start-->
-       <section class="page-header">
+    <!--Page Header Start-->
+    <section class="page-header">
         <div class="page-header__bg"></div><!-- /.page-header__bg -->
         <div class="page-header-shape-1"></div><!-- /.page-header-shape-1 -->
         <div class="page-header-shape-2"></div><!-- /.page-header-shape-2 -->
@@ -24,4 +24,32 @@
             </div>
         </div>
     </section>
-<?php include ("footer.php") ?>
+
+    <div class="container" style="padding: 75px 0">
+        <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_ef2bf"></div>
+  <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "in",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "container_id": "tradingview_ef2bf"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
+        </div>
+        <?php include("footer.php") ?>
--- metatrader-5-windows.php
+++ metatrader-5-windows.php
@@ -24,4 +24,92 @@
             </div>
         </div>
     </section>
+    <section>
+        <div class="container" style="padding-top: 70px;">
+            <div class="welcome-one__right">
+                <h2 class="welcome-one__title">METATRADER 5(windows)</h2>
+                <p class="welcome-one__text">
+                MetaTrader 5 (MT5) for Windows is a powerful and widely used trading platform designed for trading in various financial markets, including forex, stocks, commodities, and cryptocurrencies. It is the successor to MetaTrader 4 and offers a range of features and tools to meet the needs of both novice and experienced traders.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="real-world" style="background-color: #FFF">
+        <div class="real-world-shape wow slideInLeft" data-wow-delay="100ms" data-wow-duration="2500ms" style="
+            background-image: url(assets/images/shapes/real-world-shape.png);">
+        </div>
+        <div class="container">
+            <div class="section-title text-center">
+                <h2 class="section-title__title" style="color: black">Features of MetaTrader 5 for Windows:</h2>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>User-Friendly Interface</a>
+                        </h2>
+                        <p> MT5 has an intuitive and easy-to-navigate interface, making it accessible for traders of all experience levels. The layout can be customized to suit individual preferences, allowing for a personalized trading environment.</p>
+                        <span class="icon-wealth"></span>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>Multiple Asset Classes
+                            </a>
+                        </h2>
+                        <p>Traders can access a wide range of financial instruments, including currency pairs, stocks, indices, commodities, and cryptocurrencies, all within the same platform.</p><br>
+                        <br>
+                        <span class="icon-data-analytics"></span>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>Advanced Charting Tools</a>
+                        </h2>
+                        <p>MT5 provides advanced charting capabilities with multiple timeframes and various technical indicators. Traders can conduct in-depth technical analysis to identify market trends and potential entry and exit points.</p>
+                        <span class="icon-report"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>One-Click Trading</a>
+                        </h2>
+                        <p>The platform supports one-click trading, allowing traders to execute trades quickly and efficiently, especially during fast-moving market conditions.</p><br><br><br><br>
+                        <span class="icon-wealth"></span>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>Trading Orders
+                            </a>
+                        </h2>
+                        <p>MT5 offers various types of trading orders, such as market orders, pending orders (limit, stop, and stop-limit), and trailing stop orders. This flexibility enables traders to implement different trading strategies.</p>
+                        <br>
+                        <span class="icon-data-analytics"></span>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                    <div class="real-world__single">
+                        <h2 class="real-world__title">
+                            <a>Automated Trading</a>
+                        </h2>
+                        <p> Traders can use Expert Advisors (EAs), which are automated trading robots, to execute trades based on pre-defined conditions and strategies.</p><br><br>
+                        <span class="icon-report"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <?php include ("footer.php") ?>
