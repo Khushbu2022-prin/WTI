084d983b1fdcd0e0d3337af849c827a14026f571
ramaprixim <rama.prixim@gmail.com>
2023-07-27 14:52:21 +0530

    padding

---
+++ Terms-Conditions.php
@@ -0,0 +1,62 @@
+<?php include('header.php')?>
+
+    <div class="preloader">
+        <div class="preloader__image"></div>
+    </div>
+    <div class="page-wrapper">
+        
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div>
+        </div>
+       <!--Page Header Start-->
+       <section class="page-header">
+        <div class="page-header__bg"></div><!-- /.page-header__bg -->
+        <div class="page-header-shape-1"></div><!-- /.page-header-shape-1 -->
+        <div class="page-header-shape-2"></div><!-- /.page-header-shape-2 -->
+        <div class="page-header-shape-3"></div><!-- /.page-header-shape-3 -->
+        <div class="container">
+            <div class="page-header__inner">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>/</span></li>
+                    <li>About Us</li>
+                </ul>
+                <h2>Terms and Conditions</h2>
+            </div>
+        </div>
+    </section>
+    <!--Page Header End-->
+    
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="about__right">
+                        <div class="about__icon-box">
+                            <div class="">
+                            <h4>OWNERSHIP OF SITE</h4>
+                            <br>
+                                <p>WTI owns and maintains this site. No act of downloading or otherwise copying from this site will transfer title to any software or material from this site to you. Anything that you transmit to this site becomes the property of WTI, may be used by WTI for any lawful purpose, and is further subject to disclosure as deemed appropriate by WTI, including to any legal or regulatory authority to which WTI is subject. WTI reserves all rights with respect to copyright and trademark ownership of all material at this site, and will enforce such rights to the full extent of the law.</p>
+                            </div>
+                        </div>
+                        <div class="about__icon-box">
+                            <div class="">
+                            <h4>COPYRIGHT</h4>
+                            <br>
+                                <p>The material contained in the website, including but not limited to all design, text, videos, sound recordings, and images, are owned, except as otherwise expressly stated, by WTI. Except as otherwise expressly stated herein, they may not be copied, transmitted, displayed, performed, distributed (for compensation or otherwise), licensed, altered, framed, stored for subsequent use, or otherwise used in whole or in part in any manner without WTI's prior written consent.</p>
+                            </div>
+                        </div>
+                        <div class="about__icon-box">
+                            <div class="">
+                            <h4>SECURITY</h4>
+                            <br>
+                                <p>If you communicate with WTI by e-mail, you should note that the security of Internet e-mail is uncertain. By sending sensitive or confidential e-mail messages which are not encrypted you accept the risks of such uncertainty and possible lack of confidentiality over the Internet. The Internet is not 100% safe and someone may be able to intercept and read your details.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+     <?php include('footer.php') ?> 
+
